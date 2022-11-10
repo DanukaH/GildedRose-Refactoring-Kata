3ea207112c26355d316c7ed2d236419382e86580
DanukaH <danunilminda@yahoo.com>
2022-11-10 00:52:59 +0000

    Move module

---
+++ ruby/lib/gilded_rose/updater.rb
@@ -0,0 +1,72 @@
+class GildedRose
+  module Updater
+    class AgedBrie
+      attr_accessor :item
+
+      def initialize(item)
+        @item = item
+      end
+
+      def update_quality
+        item.quality += 1 if item.quality < 50
+        item.sell_in -= 1
+        item.quality += 1 if item.sell_in < 0 && item.quality < 50
+      end
+    end
+
+    class BackstagePasses
+      attr_accessor :item
+
+      def initialize(item)
+        @item = item
+      end
+
+      def update_quality
+        item.sell_in -= 1
+        return if item.quality >= 50
+        return item.quality = 0 if item.sell_in < 0
+
+        item.quality += 1
+        item.quality += 1 if item.sell_in < 10
+        item.quality += 1 if item.sell_in < 5
+      end
+    end
+
+    class Sulfuras
+      attr_accessor :item
+
+      def initialize(item)
+        @item = item
+      end
+
+      def update_quality
+      end
+    end
+
+    class NormalItem
+      attr_accessor :item
+
+      def initialize(item)
+        @item = item
+      end
+
+      def update_quality
+        item.sell_in -= 1
+        return if item.quality == 0
+
+        item.quality -= 1
+        item.quality -= 1 if item.sell_in <= 0
+      end
+    end
+
+    NAME_TO_UPDATE = {
+      'Aged Brie' => AgedBrie,
+      'Sulfuras, Hand of Ragnaros' => Sulfuras,
+      'Backstage passes to a TAFKAL80ETC concert' => BackstagePasses
+    }
+
+    def self.for(item)
+      NAME_TO_UPDATE.fetch(item.name, NormalItem).new(item)
+    end
+  end
+end
